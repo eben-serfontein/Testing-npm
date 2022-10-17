6328f04a4fff876cb125a92ba5d46ac6f6609ab3
Eben Serfontein <ebenserf@gmail.com>
2022-10-18 09:35:06 +1300

    website works now yay

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const server = express()
 server.use(express.static('public')) // lets you link directly to static files in 'public' folder
 server.use(express.urlencoded({ extended: true })) // lets you take things from browser entries
 
-// Post the user's new dog name
-server.post('/name-dog', (req, res) => {
-  let dogName = req.body.dogName
+//
+server.get('/name-dog', (req, res) => {
   res.sendFile(__dirname + '/dog-name.html')
 })
 
+// Post the user's new dog name
+server.post('/name-dog', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
 module.exports = server
+
+Arr.map((banana) => {
+  return banana++
+})
